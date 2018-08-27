4caf1503127258ec4b96f474989a0241da43bd23
Kim HyunWoo <stories2@naver.com>
2018-08-27 16:59:40 +0900

    AuthController: return user's token

--- ASP-NET-JWT/Controllers/AuthController.cs
+++ ASP-NET-JWT/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ASP_NET_JWT.Models;
+using ASP_NET_JWT.Settings;
+using ASP_NET_JWT.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,6 +16,52 @@ namespace ASP_NET_JWT.Controllers
         public IHttpActionResult test()
         {
             return Ok();
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Login([FromBody]User user)
+        {
+            var resultMsgDic = new Dictionary<string, string>();
+            try
+            {
+                User queriedUser = new UserRepository().GetUser(user.userName);
+                bool credentials = false;
+
+                if (queriedUser == null)
+                {
+                    resultMsgDic.Add(DefineManager.MSG_DIC_RESULT_KEY, DefineManager.MSG_NOT_FOUND_USER);
+                    return Request.CreateResponse(
+                        HttpStatusCode.NotFound,
+                        resultMsgDic
+                        );
+                }
+
+                credentials = queriedUser.password.Equals(user.password);
+
+                switch (credentials)
+                {
+                    case false:
+                        resultMsgDic.Add(DefineManager.MSG_DIC_RESULT_KEY, DefineManager.MSG_WRONG_USER_LOGIN_INFO);
+                        return Request.CreateResponse(
+                            HttpStatusCode.Forbidden,
+                            resultMsgDic
+                            );
+                    case true:
+                        resultMsgDic.Add(DefineManager.MSG_DIC_RESULT_KEY, TokenManager.GenerateToken(queriedUser.userName));
+                        return Request.CreateResponse(
+                            HttpStatusCode.OK,
+                            resultMsgDic
+                            );
+                }
+            }
+            catch(Exception err)
+            {
+                resultMsgDic.Add(DefineManager.MSG_DIC_RESULT_KEY, "Error: " + err);
+            }
+            return Request.CreateResponse(
+                HttpStatusCode.InternalServerError,
+                resultMsgDic
+                );
         }
     }
 }
--- ASP-NET-JWT/Settings/DefineManager.cs
+++ ASP-NET-JWT/Settings/DefineManager.cs
@@ -9,5 +9,10 @@ namespace ASP_NET_JWT.Settings
     {
         public static readonly int
             EXPIRE_TOKEN_TIME_MIN = 10;
+
+        public static readonly string
+            MSG_NOT_FOUND_USER = "The user was not found.",
+            MSG_WRONG_USER_LOGIN_INFO = "The username/password combination was wrong.",
+            MSG_DIC_RESULT_KEY = "message";
     }
 }
