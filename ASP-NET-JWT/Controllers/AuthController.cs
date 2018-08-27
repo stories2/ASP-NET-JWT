11085439d8a76d9da3d26c5378eb064143b031c6
Kim HyunWoo <stories2@naver.com>
2018-08-27 15:35:48 +0900

    Project setting <create new proj>

---
+++ ASP-NET-JWT/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_NET_JWT.Controllers
+{
+    public class AuthController : ApiController
+    {
+    }
+}
